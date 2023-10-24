4a4394d861cea39a31161c68cebf055c818a9cc6
ttowntom <117744552+ttowntom@users.noreply.github.com>
2023-10-24 15:58:08 +0200

    added featured cards

--- js/ui/index.js
+++ js/ui/index.js
@@ -2,3 +2,4 @@ export * from "./productsPage/index.js";
 export * from "./productPage/index.js";
 export * from "./breadcrumbs/index.js";
 export * from "./nav/index.js";
+export * from "./home/index.js";
